821476f065d8eee240cd9a2facb5615eb58ccf1d
Nick-the-BinaryTree <Nick-the-BinaryTree@users.noreply.github.com>
2017-01-13 17:31:07 -0500

    Add files via upload

--- ComGen.js
+++ ComGen.js
@@ -2087,8 +2087,8 @@ function ComGen () {
 	(function () {
 		var random = {};
 		__nest__ (random, '', __init__ (__world__.random));
-		var countries = list (['Afghanistan', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegowina', 'Botswana', 'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Congo, the Democratic Republic of the', 'Cook Islands', 'Costa Rica', "Cote d'Ivoire", 'Croatia (Hrvatska)', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'France Metropolitan', 'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard and Mc Donald Islands', 'Holy See (Vatican City State)', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran (Islamic Republic of)', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', "Korea, Democratic People's Republic of", 'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao, People's Democratic Republic", 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libyan Arab Jamahiriya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia, The Former Yugoslav Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia (Slovak Republic)', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia and the South Sandwich Islands', 'Spain', 'Sri Lanka', 'St. Helena', 'St. Pierre and Miquelon', 'Sudan', 'Suriname', 'Svalbard and Jan Mayen Islands', 'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela', 'Vietnam', 'Virgin Islands (British)', 'Virgin Islands (U.S.)', 'Wallis and Futuna Islands', 'Western Sahara', 'Yemen', 'Yugoslavia', 'Zambia', 'Zimbabwe']);
-		var committees = list ([dict ({'committee': 'Disarmament and International Security (DISEC)', 'topics': list (['Cyber Security', 'Militarization of Space', 'Terrorist Attacks'])}), dict ({'committee': 'Human Rights Council {HRC}', 'topics': list (['Israel-Palestine Conflict', 'Syrian Civil War', 'Child Soldiers'])}), dict ({'committee': 'Security Council {UNSC}', 'topics': list (['ISIS', 'Territorial Aggression', 'Nuclear Proliferation'])}), dict ({'committee': 'United Nations Economic and Social Council (ECOSOC)', 'topics': list (['Internet Censorship', 'Tariffs', 'Water Pollution'])}), dict ({'committee': 'United Nations Women', 'topics': list (['Violence Against Women', 'Gender Equality', 'Job Discrimination'])}), dict ({'committee': 'United Nations Environment Programme (UNEP)', 'topics': list (['Sinking Islands', 'Melting Glaciers', 'Destruction of Rainforests'])}), dict ({'committee': 'World Health Organization (WHO)', 'topics': list (['Access to Medicine', 'Epidemics', 'Mandatory Dentistry Laws'])}), dict ({'committee': 'Food and Agricultural Organization (FAO)', 'topics': list (['Global Food Waste', 'Global Allergy Warnings', 'Illegal Foods'])}), dict ({'committee': "United Nations Children's Fund (UNICEF)", 'topics': list (['Access to Education', 'Child Labor', 'Orphan Laws'])}), dict ({'committee': 'Office of the United Nations High Commissioner on Refugees (UNHCR)', 'topics': list (['Climate Change-Related Disasters', 'Trapped Populations', 'Right to Migration'])}), dict ({'committee': 'Legal Committee', 'topics': list (['Drone Laws', 'Legal Rebellion', 'Endangered Species Laws'])}), dict ({'committee': 'Committee on Crime Prevention and Criminial Justice (CCPCJ)', 'topics': list (['Capital Punishment', 'Cyber Crime', 'Human Trafficking'])}), dict ({'committee': 'United Nations Development Programme (UNDP)', 'topics': list (['Orphan Rights', 'Decaying Infrastructure', 'Chronic Rural Poverty'])}), dict ({'committee': 'United Nations Economic, Social, and Cultural Organization (UNESCO)', 'topics': list (['Educating Migrants', 'Equality of Education', 'Religious Tension'])})]);
+		var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+		var committees = list ([dict ({'committee': 'Disarmament and International Security (DISEC)', 'topics': list (['Cyber Security', 'Militarization of Space', 'Terrorist Attacks'])}), dict ({'committee': 'Human Rights Council (HRC)', 'topics': list (['Israel-Palestine Conflict', 'Syrian Civil War', 'Child Soldiers'])}), dict ({'committee': 'Security Council (UNSC)', 'topics': list (['ISIS', 'Territorial Aggression', 'Nuclear Proliferation'])}), dict ({'committee': 'United Nations Economic and Social Council (ECOSOC)', 'topics': list (['Internet Censorship', 'Tariffs', 'Water Pollution'])}), dict ({'committee': 'United Nations Women', 'topics': list (['Violence Against Women', 'Gender Equality', 'Job Discrimination'])}), dict ({'committee': 'United Nations Environment Programme (UNEP)', 'topics': list (['Sinking Islands', 'Melting Glaciers', 'Destruction of Rainforests'])}), dict ({'committee': 'World Health Organization (WHO)', 'topics': list (['Access to Medicine', 'Epidemics', 'Mandatory Dentistry Laws'])}), dict ({'committee': 'Food and Agricultural Organization (FAO)', 'topics': list (['Global Food Waste', 'Global Allergy Regulations', 'Illegal Foods'])}), dict ({'committee': "United Nations Children's Fund (UNICEF)", 'topics': list (['Access to Education', 'Child Labor', 'Orphan Laws'])}), dict ({'committee': 'Office of the United Nations High Commissioner on Refugees (UNHCR)', 'topics': list (['Climate Change-Related Disasters', 'Trapped Populations', 'Right to Migration'])}), dict ({'committee': 'Legal Committee', 'topics': list (['Drone Laws', 'Legal Rebellion', 'Endangered Species Laws'])}), dict ({'committee': 'Committee on Crime Prevention and Criminial Justice (CCPCJ)', 'topics': list (['Capital Punishment', 'Cyber Crime', 'Human Trafficking'])}), dict ({'committee': 'United Nations Development Programme (UNDP)', 'topics': list (['Orphan Rights', 'Decaying Infrastructure', 'Chronic Rural Poverty'])}), dict ({'committee': 'United Nations Economic, Social, and Cultural Organization (UNESCO)', 'topics': list (['Educating Migrants', 'Equality of Education', 'Religious Tension'])})]);
 		var takenCountries = list ([]);
 		var getCountry = function () {
 			if (len (takenCountries) >= len (countries)) {
@@ -2123,6 +2123,7 @@ function ComGen () {
 			if (typeof num_countries == 'undefined' || (num_countries != null && num_countries .hasOwnProperty ("__kwargtrans__"))) {;
 				var num_countries = 10;
 			};
+            takenCountries.length = 0;
 			var result = getTopic ();
 			for (var _ = 0; _ < num_countries; _++) {
 				takenCountries.append (getCountry ());
@@ -2149,12 +2150,13 @@ window ['ComGen'] = ComGen ();
 
 
 //The real JS begins
+var countriesDiv;
 window.onload = function(){
     var numCountriesBox = document.getElementById("numCountries")
     var settingsForm = document.getElementById("settings");
     var comName = document.getElementById("committee");
     var topicName = document.getElementById("topic");
-    var countriesDiv = document.getElementById("countries");
+    countriesDiv = document.getElementById("countries");
     
     settingsForm.onsubmit = function(){
         var numCountries = numCountriesBox.value;
@@ -2177,19 +2179,19 @@ window.onload = function(){
         }
         return false;
     }
-    
-    function removeCountries(){
-        while(countriesDiv.hasChildNodes()){
-            countriesDiv.removeChild(countriesDiv.lastChild);
-        }
+}
+
+function removeCountries(){
+    while(countriesDiv.hasChildNodes()){
+        countriesDiv.removeChild(countriesDiv.lastChild);
     }
-    
-    function addCountries(countries){
-        for(var i = 0; i < countries.length; i++){
-            var countryElem = document.createElement("p");
-            countryElem.className = "country";
-            countryElem.innerHTML = countries[i];
-            countriesDiv.appendChild(countryElem);
-        }
+}
+
+function addCountries(countries){
+    for(var i = 0; i < countries.length; i++){
+        var countryElem = document.createElement("p");
+        countryElem.className = "country";
+        countryElem.innerHTML = countries[i];
+        countriesDiv.appendChild(countryElem);
     }
 }
